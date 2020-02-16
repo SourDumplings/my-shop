35e3aa9a59812dca27a19a0ad0ebd9ae0f6c15ae
SourDumplings <changzheng300@foxmail.com>
2020-02-16 21:19:53 +0800

    修复了注册页的问题.

--- my-shop-web-ui/src/main/java/com/cz/my/shop/web/ui/controller/RegisterController.java
+++ my-shop-web-ui/src/main/java/com/cz/my/shop/web/ui/controller/RegisterController.java
@@ -1,5 +1,6 @@
 package com.cz.my.shop.web.ui.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * @projectName my-shop
  * @date 2020/2/16 20:43
  */
+@Controller
 public class RegisterController
 {
 
